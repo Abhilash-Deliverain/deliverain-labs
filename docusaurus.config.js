5982363aca288680fa86b8c06c1b5d1c5fb33bc1
Abhilash Rajendran <83715428+Abhilash0086@users.noreply.github.com>
2025-11-13 11:28:09 +0530

    fix: update Docusaurus URL for custom domain labs.deliverain.com

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://abhilash-deliverain.github.io',
+  url: 'https://labs.deliverain.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/deliverain-labs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
